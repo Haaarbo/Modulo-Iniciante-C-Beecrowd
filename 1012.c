dc3b4a4771e46c362683483f24f2ad23ce319e5a
Haaarbo <abrahao0707@gmail.com>
2022-06-19 18:57:40 -0400

    Update 1012.c

--- 1012.c
+++ 1012.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
- #define pi 3.14159
- 
+#define pi 3.14159
+
 int main() {
     
     double A, B, C;
+    double triangulo, circulo, trapezio, quadrado, retangulo;
     
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    triangulo = (A*C)/2;
+    circulo = pi*C*C;
+    trapezio = (A+B)*C/2;
+    quadrado = B*B;
+    retangulo = A*B;
+
+    printf("TRIANGULO: %.3f\n", triangulo);
+    printf("CIRCULO: %.3f\n", circulo);
+    printf("TRAPEZIO: %.3f\n", trapezio);
+    printf("QUADRADO: %.3f\n", quadrado);
+    printf("RETANGULO: %.3f\n", retangulo);
+
     return 0;
 }
