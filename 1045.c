706cc5c69b8767aa66b547aa4f2d3c6f8036c2df
Haaarbo <107763560+Haaarbo@users.noreply.github.com>
2022-08-01 15:41:07 -0400

    1045.c

---
+++ 1045.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double A, B, C, maior, meio, menor;
+
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    maior = (A > B) ? A : B;
+    maior = (C > maior) ? C : maior;
+
+    menor = (A < B) ? A : B;
+    menor = (C < menor) ? C : menor;
+
+    meio = (A <= B && A >= C || A <= C && A >= B) ? A : meio;
+    meio = (B <= A && B >= C || B <= C && B >= A) ? B : meio;
+    meio = (C <= A && C >= B || C <= B && C >= A) ? C : meio;
+    
+    A = maior;
+    B = meio;
+    C = menor;
+    
+    if(A >= B+C)
+        printf("NAO FORMA TRIANGULO\n");
+    
+    else
+    {
+        if(pow(A, 2) == pow(B, 2) + pow(C, 2))
+            printf("TRIANGULO RETANGULO\n");
+        
+        else if(pow(A, 2) > pow(B, 2) + pow(C, 2))
+            printf("TRIANGULO OBTUSANGULO\n");
+        
+        else
+            printf("TRIANGULO ACUTANGULO\n");
+        
+        if(A == B && B == C)
+            printf("TRIANGULO EQUILATERO\n");
+            
+        else if(A == B && A != C || B == C && B != A)
+            printf("TRIANGULO ISOSCELES\n");
+    }   
+    
+    
+
+    return 0;
+}
