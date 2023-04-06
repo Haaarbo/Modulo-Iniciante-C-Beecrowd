fcb4ba4664ed77e752481711f2610ea71b63aa12
Haaarbo <107763560+Haaarbo@users.noreply.github.com>
2023-04-06 12:52:05 -0400

    1097

---
+++ 1097.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define TAM 9
+
+int main() {
+ 
+    int I, J = 7;
+
+    for(I = 1; I <= TAM; I += 2)
+    {
+        printf("I=%d J=%d\n", I, J);
+        J--;
+        printf("I=%d J=%d\n", I, J);
+        J--;
+        printf("I=%d J=%d\n", I, J);
+        J += 4;
+    }
+    return 0;
+}
